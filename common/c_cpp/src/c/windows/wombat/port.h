72f9cc6ff143053cd3e7082dfa9cf0cb520e02c0
Glenn McClements <gmcclements@nyx.com>
2013-02-18 15:32:39 +0000

    Correcting an ambiguity in wombat\port.h in the definition of int8_t, which is currently of type 'char'. Whether the char type is signed or unsigned is compiler-specific, whereas the int8_t typedef is expected to be signed.
    Signed-off-by: Joshua Salit <jrsalit@us.ibm.com>

--- common/c_cpp/src/c/windows/wombat/port.h
+++ common/c_cpp/src/c/windows/wombat/port.h
@@ -57,7 +57,7 @@ typedef void * caddr_t;
 typedef u_long in_addr_t;
 
 typedef unsigned char    uint8_t;
-typedef char             int8_t;
+typedef signed char      int8_t;
 typedef unsigned short   uint16_t;
 typedef short            int16_t;
 typedef unsigned int     uint32_t;
