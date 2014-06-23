6f6ad762f48349a679ac096f41da4c2853e29b69
Ian Bell <ibell@nyx.com>
2014-06-23 16:44:04 +0100

    MAMAC: Update Doxygen comments - clientmanage.h and conflation
    Updated comments for conflation and clientmanage.h header files
    to improve reference documentaion.
    
    Signed-off-by: Ian Bell <ibell@nyx.com>

--- mama/c_cpp/src/c/mama/conflation/connection.h
+++ mama/c_cpp/src/c/mama/conflation/connection.h
@@ -35,84 +35,166 @@ extern "C" {
 #endif
 
 /**
- * Free the connection.
+ * @brief Free the connection.
+ *
+ * @param[in] connection The mama connection
+ *
+ * @return mama_status return code MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_free (mamaConnection connection);
 
 /**
- * Convenience method for returnining the transport.
+ * @brief Convenience method for returnining the transport.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] result     Returning the associated transport.
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK       
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getTransport (mamaConnection connection, mamaTransport *result);
 
 /**
- * Return the IP address of the connected client.
+ * @brief Return the IP address of the connected client.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] address    Returning the associated IP address
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getIpAddress (mamaConnection connection, const char** address);
 
 /**
- * Return the port of the connected client. This will usually be an ephemeral
+ * @brief Return the port of the connected client. This will usually be an ephemeral
  * port selected by the client's ip stack.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] port       Returning the associated port
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getPort (mamaConnection connection, uint16_t* port);
 
 /*
- * Return the user name of the connected client.
+ * @brief Return the user name of the connected client.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] userName   Returning the associated user name
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getUserName (mamaConnection connection, const char** userName);
 
 /**
- * Return the application name of the connected client.
+ * @brief Return the application name of the connected client.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] appName    Returning the associated application name
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getAppName (mamaConnection connection, const char** appName);
 
 /**
- * Return the maximum allowable outbound queue size for the client.
+ * @brief Return the maximum allowable outbound queue size for the client.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] queueSize  Returning the associated maximum queue size
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getMaxQueueSize(mamaConnection connection, uint32_t *queueSize);
 
 /**
- * Get the number of items in the queue when structure was created.
+ * @brief Get the number of items in the queue when structure was created.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] numItems   Returning the associated number of items in the 
+ *                        queue
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getQueueSize (mamaConnection connection, uint32_t *numItems);
 
 /**
- * Get the number of messages sent on the connection.
+ * @brief Get the number of messages sent on the connection.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] msgCount   Returning the number of messages per connection
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK 
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getMsgCount (mamaConnection connection, uint32_t *msgCount);
 
 /**
- * Get the number of bytes sent on the connection.
+ * @brief Get the number of bytes sent on the connection.
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] bytesSent  Returning the number of bytes sent per
+ *                        connection
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_OK
  */
 MAMAExpDLL
 mama_status
 mamaConnection_getBytesSent (mamaConnection connection, uint32_t *bytesSent);
 
 /**
- * Convert a connection to a string.
+ * @brief Convert a connection to a string.
+ *
+ * @param[in]  connection The mama connection
+ *
+ * @return String representation of the mama connection object
  */
 MAMAExpDLL
 const char*
 mamaConnection_toString (mamaConnection connection);
 
-/* Result is 0 if connection is not intercepted */
+/**
+ * @brief Check if the conneciton is intercepted
+ *
+ * @param[in]  connection The mama connection
+ * @param[out] result     Returns 0 if connection is not intercepted.
+ *
+ * @return mama_status return code can be one of:
+ *              MAMA_STATUS_NULL_ARG
+ *              MAMA_STATUS_NOT_INSTALLED
+ */
 MAMAExpDLL
 mama_status
 mamaConnection_isIntercepted (mamaConnection connection, uint8_t* result);
