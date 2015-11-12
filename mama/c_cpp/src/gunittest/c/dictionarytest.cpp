cc5bf85aa3afaa337a82ffa33b5219bdf5cbeff4
reed-jpm-alpert <reed.alpert@jpmorgan.com>
2015-11-12 14:48:09 -0500

    Dictionary: add fid and name to msg for unknown type
    In populateMessageFromDictionary() if the dict has an unknown type the field is discarded but the msg only says it was an unknown type. The patch will add the fid and name to the log msg to help locate the field. Includes new unit test.
    
    Signed-off-by: Reed Alpert <reed.alpert@gmail.com>

---
+++ mama/c_cpp/src/gunittest/c/dictionarytest.cpp
@@ -0,0 +1,104 @@
+/* $Id$
+ *
+ * OpenMAMA: The open middleware agnostic messaging API
+ * Copyright (C) 2011 NYSE Technologies, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+
+#include <gtest/gtest.h>
+#include "MainUnitTestC.h"
+#include "wombat/wConfig.h"
+#include "mama/mama.h"
+#include "mama/status.h"
+#include "mama/types.h"
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+
+
+class MamaDictionaryTestC : public ::testing::Test
+{	
+protected:
+    MamaDictionaryTestC();          
+    virtual ~MamaDictionaryTestC(); 
+
+    virtual void SetUp();        
+    virtual void TearDown ();    
+public:
+    MamaDictionaryTestC *m_this;
+    mamaBridge mBridge;
+};
+
+MamaDictionaryTestC::MamaDictionaryTestC()
+{
+}
+
+MamaDictionaryTestC::~MamaDictionaryTestC()
+{
+}
+
+void MamaDictionaryTestC::SetUp(void)
+{	
+    mama_loadBridge (&mBridge, getMiddleware());
+    mama_open (); 
+}
+
+void MamaDictionaryTestC::TearDown(void)
+{
+    mama_close ();
+}
+
+/* ************************************************************************* */
+/* Test Functions */
+/* ************************************************************************* */
+
+/*  Description: Create a mamaDictionary from a file and write to a msg.
+ *  ISSUE 85
+ *  The dictionary file includes a field type of UNKNOWN, which is logged with name and fid.
+ *                   
+ *  Expected Result: MAMA_STATUS_OK
+ */
+TEST_F (MamaDictionaryTestC, LoadFromFileAndWriteToMsg)
+{
+	mamaDictionary dictionary;
+	mamaMsg msg;
+	char buf[1024];
+	const char* path = getenv("WOMBAT_PATH");
+	size_t dictionarySize;
+	size_t msgSize;
+    
+    ASSERT_EQ (MAMA_STATUS_OK, mamaDictionary_create(&dictionary));
+
+	ASSERT_NE (path, (const char*) NULL);
+	snprintf(buf, sizeof(buf), "%s/dictionary1.txt", path);
+    ASSERT_EQ (MAMA_STATUS_OK, mamaDictionary_populateFromFile(dictionary, buf));
+
+	ASSERT_EQ (MAMA_STATUS_OK, mamaDictionary_getDictionaryMessage(dictionary, &msg));
+
+	ASSERT_EQ (MAMA_STATUS_OK, mamaDictionary_getSize(dictionary, &dictionarySize));
+
+	ASSERT_EQ (MAMA_STATUS_OK, mamaMsg_getNumFields(msg, &msgSize));
+
+	/* Expect one less in msg since there is an unknown in the dictionary */
+	ASSERT_EQ (dictionarySize-1, msgSize);
+
+	ASSERT_EQ (MAMA_STATUS_OK, mamaMsg_destroy(msg));
+
+	ASSERT_EQ (MAMA_STATUS_OK, mamaDictionary_destroy(dictionary));
+}
+
