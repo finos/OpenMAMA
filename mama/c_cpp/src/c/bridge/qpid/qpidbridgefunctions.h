f4ad5eb2fe799b6bc9279fba36be947f9f41da64
Frank Quinn <fquinn.ni@gmail.com>
2016-03-18 11:53:03 +0000

    QPID: Added implementation for publisher callbacks in qpid bridge
    An implementation has now been added to publisher callbacks though
    the onError callback remains unwired pending the update for
    https://github.com/OpenMAMA/OpenMAMA/issues/65.
    
    Disabled EventSendWithCallbacksBadSource and
    EventSendWithCallbacksBadSource unit tests pending further
    investigation. They do not appear to be fair as they depend on the
    underlying implementation somehow already having an environment
    set up which will definitely fail to send this one particular
    message... somehow. Which conditions constitude a fail and how the
    environment will figure out how to let "just this" message error
    will always be implementation specific so I'm not sure if we will
    ever have a good answer for this (i.e. we may delete these tests
    entirely).
    
    Signed-off-by: Frank Quinn <fquinn.ni@gmail.com>

--- mama/c_cpp/src/c/bridge/qpid/qpidbridgefunctions.h
+++ mama/c_cpp/src/c/bridge/qpid/qpidbridgefunctions.h
@@ -1153,6 +1153,31 @@ qpidBridgeMamaPublisher_sendFromInbox (publisherBridge publisher,
                                        mamaInbox       inbox,
                                        mamaMsg         msg);
 
+/**
+ * This method informs the middleware bridge implementation of the callbacks
+ * to use when various situations are encountered as defined by
+ * mamaPublisherCallbacks.
+ *
+ * In qpid, this won't wire error callbacks yet because trackers have not yet
+ * been written for error conditions. That will tie in with github issue
+ * https://github.com/OpenMAMA/OpenMAMA/issues/65.
+ *
+ * Requirement:      Optional
+ *
+ * @param publisher  This is the qpid publisher implementation to use
+ * @param queue      The publisher queue
+ * @param cb         The callbacks to trigger
+ * @param closure    The closure to send back to any callbacks triggered
+ *
+ * @return mama_status indicating whether the method succeeded or failed.
+ */
+MAMAExpDLL
+extern mama_status
+qpidBridgeMamaPublisher_setUserCallbacks (publisherBridge         publisher,
+                                          mamaQueue               queue,
+                                          mamaPublisherCallbacks* cb,
+                                          void*                   closure);
+
 
 /*=========================================================================
   =                    Functions for the mamaInbox                        =
