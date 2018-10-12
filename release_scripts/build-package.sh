7b98e38874806a9b4110a88e2c6292d937fb18d5
Frank Quinn <fquinn@cascadium.io>
2018-10-12 22:15:05 +0100

    Added initial docker files for performing builds
    Going down the docker road allows us to provide packages outside of the
    redhat mock ecosystem by leveraging FPM. This change involves:
    
    1. A docker file to recreate each OS environment
    2. A script for installing dependencies on a fresh system
    3. A script for actually performing the package
    
    Note that the docker build creates the build environment including
    dependencies and builds the RPM. The docker run command simply packages
    up the results and may be used in conjunction with shared volumes to
    make the package accessible via the host. E.g.
    
        docker build -t omc7 -f release_scripts/Dockerfile . --build-arg IMAGE=centos:7
        docker run -e VERSION=6.2.3 -v release:/apps/release --rm omc7
    
    Still a bit of work to do around "where to put things" but should mostly
    be there.
    
    Signed-off-by: Frank Quinn <fquinn@cascadium.io>

---
+++ release_scripts/build-package.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Make all unhandled erroneous return codes fatal
+set -e
+
+# Globals
+INSTALL_DIR=${DEPS_DIR:-/app/install}
+
+# Constants
+FEDORA=Fedora
+RHEL=CentOS
+UBUNTU=Ubuntu
+
+test -z "$VERSION" && echo "VERSION must be specified!" && exit $LINENO
+
+if [ -z "$IS_DOCKER_BUILD" ]
+then
+    echo
+    echo "------------------------------- DRAGON ALERT --------------------------------"
+    echo "IS_DOCKER_BUILD environment not set. This should be set before launching this"
+    echo "script as the root user IN AN ISOLATED DOCKER ENVIRONMENT. It is highly "
+    echo "recommended not to run this script in any other environment. Obviously we"
+    echo "cannot stop you... but if you try it, you're on your own..."
+    echo "-----------------------------------------------------------------------------"
+    echo
+    exit $LINENO
+fi
+
+if [ -f /etc/redhat-release ]
+then
+    DISTRIB_RELEASE=$(cat /etc/redhat-release | tr " " "\n" | egrep "^[0-9]")
+    if [ -f /etc/fedora-release ]
+    then
+        DISTRIB_ID=$FEDORA
+    else
+        DISTRIB_ID=$RHEL
+    fi
+    PACKAGE_TYPE=rpm
+    DEPENDS_FLAGS="-d libuuid -d qpid-proton-c -d libevent -d ncurses -d apr -d java-1.8.0-openjdk -d libnsl2"
+fi
+
+if [ -f /etc/lsb-release ]
+then
+    # Will set DISTRIB_ID and DISTRIB_RELEASE
+    source /etc/lsb-release
+    PACKAGE_TYPE=deb
+    DEPENDS_FLAGS="-d uuid -d libevent -d libzmq3 -d openjdk-8-jdk -d ncurses -d libapr1"
+fi
+
+if [ -z "$DISTRIB_RELEASE" ]
+then
+    echo "Unsupported distro found: $(cat /etc/*-release)" && exit $LINENO
+fi
+
+fpm -s dir -t $PACKAGE_TYPE -C $INSTALL_DIR --name openmama --version $VERSION --iteration 1 \
+        $DEPENDS_FLAGS --description "OpenMAMA high performance Market Data API" .
---
+++ release_scripts/install-dependencies.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# Make all unhandled erroneous return codes fatal
+set -e
+
+# Globals
+export SDKMAN_DIR=${SDKMAN_DIR:-/usr/local/sdkman}
+VERSION_GTEST=${VERSION_GTEST:-1.8.0}
+VERSION_APR=${VERSION_APR:-1.6.3}
+VERSION_QPID=${VERSION_QPID:-0.24.0}
+DEPS_DIR=${DEPS_DIR:-/app/deps}
+
+# Constants
+FEDORA=Fedora
+RHEL=CentOS
+UBUNTU=Ubuntu
+
+if [ -z "$IS_DOCKER_BUILD" ]
+then
+    echo
+    echo "------------------------------- DRAGON ALERT --------------------------------"
+    echo "IS_DOCKER_BUILD environment not set. This should be set before launching this"
+    echo "script as the root user IN AN ISOLATED DOCKER ENVIRONMENT. It is highly "
+    echo "recommended not to run this script in any other environment. Obviously we"
+    echo "cannot stop you... but if you try it, you're on your own..."
+    echo "-----------------------------------------------------------------------------"
+    echo
+    exit $LINENO
+fi
+
+if [ -f /etc/redhat-release ]
+then
+    DISTRIB_RELEASE=$(cat /etc/redhat-release | tr " " "\n" | egrep "^[0-9]")
+    if [ -f /etc/fedora-release ]
+    then
+        DISTRIB_ID=$FEDORA
+    else
+        DISTRIB_ID=$RHEL
+    fi
+fi
+
+if [ -f /etc/lsb-release ]
+then
+    # Will set DISTRIB_ID and DISTRIB_RELEASE
+    source /etc/lsb-release
+fi
+
+if [ -z "$DISTRIB_RELEASE" ]
+then
+    echo "Unsupported distro found: $(cat /etc/*-release)" && exit $LINENO
+fi
+
+if [ "$DISTRIB_ID" = "$FEDORA" ]
+then
+    echo "Installing fedora specific dependencies"
+    yum install -y libnsl2-devel libffi-devel ruby-devel rubygems redhat-rpm-config
+fi
+
+if [ "$DISTRIB_ID" = "$RHEL" ]
+then
+    echo "Installing CentOS / RHEL specific dependencies"
+    yum install -y epel-release gcc make rpm-build which
+fi
+
+if [ "$DISTRIB_ID" = "$RHEL" ] || [ "$DISTRIB_ID" = "$FEDORA" ]
+then
+    yum install -y zlib-devel openssl-devel zip unzip make \
+	    java-1.8.0-openjdk-devel libuuid-devel flex doxygen \
+	    qpid-proton-c-devel libevent-devel ncurses-devel \
+	    apr-devel wget curl cmake gcc-c++ libuuid qpid-proton-c \
+	    libevent ncurses apr
+fi
+
+if [ "$DISTRIB_ID" = "$UBUNTU" ]
+then
+    apt-get update -qq
+    apt-get install -qq -y ruby ruby-dev rubygems build-essential \
+	    zip unzip curl git flex uuid-dev libevent-dev \
+	    cmake git libzmq3-dev openjdk-8-jdk ncurses-dev \
+	    unzip valgrind libapr1-dev
+fi
+
+test -d $DEPS_DIR || mkdir -p $DEPS_DIR
+
+# Centos version specific dependencies (ruby is too old for FPM)
+if [ "$DISTRIB_ID" = "$RHEL" ] && [ "${DISTRIB_RELEASE:0:1}"  = "6" ]
+then
+    cd $DEPS_DIR
+    curl -sL http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.2.tar.gz | tar xz
+    cd ruby-2.1.2
+    ./configure --prefix=/usr
+    make
+    make install && gem update --system
+elif [ "$DISTRIB_ID" = "$RHEL" ]
+then
+    yum install -y ruby-devel rubygems
+fi
+
+# Install gradle
+curl -s "https://get.sdkman.io" | bash
+source "$SDKMAN_DIR/bin/sdkman-init.sh"
+sdk install gradle
+
+# Install FPM for packaging up
+gem install --no-ri --no-rdoc fpm
+
+# Gtest is best always getting built
+cd $DEPS_DIR
+curl -sL http://github.com/google/googletest/archive/release-$VERSION_GTEST.tar.gz | tar xz
+cd googletest-release-$VERSION_GTEST
+mkdir bld
+cd bld
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make && make install
+
+# Ubuntu doesn't have this as a package yet so need to build
+if [ "$DISTRIB_ID" = "$UBUNTU" ]
+then
+    cd $DEPS_DIR
+    curl -sL http://github.com/apache/qpid-proton/archive/$VERSION_QPID.tar.gz | tar xz
+    cd qpid-proton-$VERSION_QPID
+    mkdir bld
+    cd bld
+    cmake -DBUILD_TESTING:BOOL=OFF -DBUILD_CPP:BOOL=OFF -DCMAKE_INSTALL_PREFIX=/usr ..
+    make
+    make install
+fi
