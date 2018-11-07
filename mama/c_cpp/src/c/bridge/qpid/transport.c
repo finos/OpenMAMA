6d1647a02619e9d5d6c545d28acbbccb332ea543
STANISLAVBELOTSERKOVSKIY <stanislav.belotserkovskiy@jpmchase.com>
2018-11-07 13:53:54 -0500

    set defoutUrl for p2p transport type

--- mama/c_cpp/src/c/bridge/qpid/transport.c
+++ mama/c_cpp/src/c/bridge/qpid/transport.c
@@ -368,7 +368,7 @@ qpidBridgeMamaTransport_create (transportBridge*    result,
     else if (0 == strcmp (tportType, CONFIG_VALUE_TPORT_TYPE_P2P))
     {
         impl->mQpidTransportType = QPID_TRANSPORT_TYPE_P2P;
-        defOutUrl = NULL;
+		defOutUrl = DEFAULT_OUTGOING_URL;
     }
     else
     {
