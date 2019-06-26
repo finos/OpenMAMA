d1db40cffa297818247eb00734a8003107c21c70
Frank Quinn <fquinn@cascadium.io>
2019-06-26 18:56:26 +0100

    Updated version information to 6.3.0
    Signed-off-by: Frank Quinn <fquinn@cascadium.io>

--- mama/dotnet/src/cs/MamaVersion.cs
+++ mama/dotnet/src/cs/MamaVersion.cs
@@ -5,8 +5,8 @@ namespace Wombat
     public class MamaVersion
     {
         public static int MAMADOTNET_VERSION_MAJOR = 6;
-        public static int MAMADOTNET_VERSION_MINOR = 2;
-        public static string MAMADOTNET_VERSION_RELEASE = "3";
-        public static string MAMADOTNET_VERSION = "mamadotnet 6.2.3";
+        public static int MAMADOTNET_VERSION_MINOR = 3;
+        public static string MAMADOTNET_VERSION_RELEASE = "0";
+        public static string MAMADOTNET_VERSION = "mamadotnet 6.3.0";
     }
 }
