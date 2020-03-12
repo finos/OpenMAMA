2f97e4a10bcead639e6c63f2ebb854c54dc893ae
Bill Torpey <bill.torpey@itiviti.com>
2020-03-12 14:58:09 -0400

    add more uuid_ functions

--- common/c_cpp/src/c/linux/wombat/wUuid.h
+++ common/c_cpp/src/c/linux/wombat/wUuid.h
@@ -27,10 +27,13 @@
 
 typedef uuid_t wUuid;
 
-#define wUuid_generate_time uuid_generate_time
+#define wUuid_generate              uuid_generate
+#define wUuid_generate_time         uuid_generate_time
+#define wUuid_generate_time_safe    uuid_generate_time_safe
+#define wUuid_generate_random       uuid_generate_random
 
 #define wUuid_unparse uuid_unparse
 
-#define wUuid_clear(UUID) uuid_clear(UUID);
+#define wUuid_clear(UUID) uuid_clear(UUID)
 
 #endif /* WUUID_H__ */
