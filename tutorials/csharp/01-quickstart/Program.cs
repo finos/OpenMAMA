4cb6d5a668aa98062cf1560383d6f8da8c2fe51e
Frank Quinn <fquinn@cascadium.io>
2020-09-03 20:49:28 +0100

    Added C# quickstart code
    Signed-off-by: Frank Quinn <fquinn@cascadium.io>

---
+++ tutorials/csharp/01-quickstart/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Wombat;
+
+
+namespace _01_quickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MamaBridge m_bridge = Mama.loadBridge("qpid");
+            Console.WriteLine(Mama.getVersion(m_bridge));
+        }
+    }
+}
