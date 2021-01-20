075ac02caca7a3841124c35fed6c6fdd19d8fb03
fquinner <fquinn@cascadium.io>
2021-01-20 21:06:32 +0000

    Update ci-run.bat
    Updated vcpkg paths

--- devops/build/ci-run.bat
+++ devops/build/ci-run.bat
@@ -7,8 +7,7 @@ vcpkg install qpid-proton:%PLATFORM%-windows libevent:%PLATFORM%-windows apr:%PL
 call RefreshEnv.cmd
 mkdir build || goto error
 cd build || goto error
-REM -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_CSHARP=ON -DWITH_UNITTEST=ON -DWITH_JAVA=ON -DPROTON_ROOT=C:/tools/vcpkg/installed/%PLATFORM%-windows/ -DGTEST_ROOT=C:/tools/vcpkg/installed/%PLATFORM%-windows/ -DINSTALL_RUNTIME_DEPENDENCIES=ON -DCMAKE_INSTALL_PREFIX="%OPENMAMA_INSTALL_DIR%" -G "%GENERATOR%" -DAPR_ROOT=C:/tools/vcpkg/installed/%PLATFORM%-windows/ .. || goto error
+cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_CSHARP=ON -DWITH_UNITTEST=ON -DWITH_JAVA=ON -DPROTON_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DGTEST_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DINSTALL_RUNTIME_DEPENDENCIES=ON -DCMAKE_INSTALL_PREFIX="%OPENMAMA_INSTALL_DIR%" -G "%GENERATOR%" -DAPR_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DCMAKE_TOOLCHAIN_FILE=c:/vcpkg/scripts/buildsystems/vcpkg.cmake .. || goto error
 cmake --build . --config RelWithDebInfo --target install || goto error
 cd .. || goto error
 python release_scripts\ci-run.py || goto error
