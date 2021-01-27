93ea19a678375d67ee405bab7b7b152fdf21242a
Frank Quinn <fquinn@cascadium.io>
2021-01-27 22:02:42 +0000

    Made choco quieter and echo PATH

--- devops/build/ci-run.bat
+++ devops/build/ci-run.bat
@@ -2,13 +2,14 @@ set PATH=%JAVA_HOME_OVERRIDE%\bin;%PATH%
 set /p VERSION=<%GITHUB_WORKSPACE%\VERSION
 set OPENMAMA_INSTALL_DIR=%GITHUB_WORKSPACE%\openmama-%VERSION%.win.%PLATFORM%
 
-choco install -y unzip gow wget gradle nunit-console-runner nunit-extension-nunit-v2-driver cmake || goto error
+choco install --no-progress -y unzip gow wget gradle nunit-console-runner nunit-extension-nunit-v2-driver cmake || goto error
 vcpkg install qpid-proton:%PLATFORM%-windows libevent:%PLATFORM%-windows apr:%PLATFORM%-windows gtest:%PLATFORM%-windows || goto error
 msiexec.exe /I https://github.com/nunit-legacy/nunitv2/releases/download/2.7.1/NUnit-2.7.1.msi /QN || goto error
 call RefreshEnv.cmd
 mkdir %GITHUB_WORKSPACE%\build || goto error
 cd %GITHUB_WORKSPACE%\build || goto error
 cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -DWITH_CSHARP=ON -DWITH_UNITTEST=ON -DWITH_JAVA=ON -DPROTON_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DGTEST_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DINSTALL_RUNTIME_DEPENDENCIES=ON -DCMAKE_INSTALL_PREFIX="%OPENMAMA_INSTALL_DIR%" -G "%GENERATOR%" -DAPR_ROOT=C:/vcpkg/installed/%PLATFORM%-windows/ -DCMAKE_TOOLCHAIN_FILE=c:/vcpkg/scripts/buildsystems/vcpkg.cmake %EXTRA_ARGS% .. || goto error
+echo PATH=%PATH%
 call build_dll_path_RelWithDebInfo.bat
 cmake --build . --config RelWithDebInfo --target install || goto error
 ctest . -C RelWithDebInfo
