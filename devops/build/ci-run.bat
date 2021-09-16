9dfbc4fdc396b795b5bbb4f0d1c483953da52610
Frank Quinn <fquinn@cascadium.io>
2021-09-16 19:10:35 +0100

    Made windows ci test failures fatal
    Signed-off-by: Frank Quinn <fquinn@cascadium.io>

--- devops/build/ci-run.bat
+++ devops/build/ci-run.bat
@@ -12,9 +12,9 @@ REM Set PATH before adding to it since it's absurdly long in github actions and 
 set PATH=C:\Program Files (x86)\Gow\bin;%JAVA_HOME%;C:\Program Files\CMake\bin;C:\Program Files\Docker;C:\Program Files\Git\bin;C:\Program Files\Git\cmd;C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin;C:\Program Files\Java\jdk8u275-b01\bin;C:\Program Files\OpenSSL\bin;C:\Program Files\PowerShell\7\;C:\Program Files\TortoiseSVN\bin;C:\Program Files\dotnet;C:\ProgramData\Chocolatey\bin;C:\ProgramData\chocolatey\lib\ghc.8.10.2.2\tools\ghc-8.10.2\bin;C:\ProgramData\chocolatey\lib\maven\apache-maven-3.6.3\bin;C:\ProgramData\chocolatey\lib\pulumi\tools\Pulumi\bin;C:\ProgramData\kind;C:\hostedtoolcache\windows\Python\3.7.9\x64;C:\hostedtoolcache\windows\Python\3.7.9\x64\Scripts;C:\hostedtoolcache\windows\Ruby\2.5.8\x64\bin;C:\hostedtoolcache\windows\go\1.14.13\x64\bin;C:\hostedtoolcache\windows\stack\2.5.1\x64;C:\mysql-5.7.21-winx64\bin;C:\npm\prefix;C:\vcpkg;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\windows\system32;c:\tools\php
 call build_dll_path_RelWithDebInfo.bat
 cmake --build . --config RelWithDebInfo --target install || goto error
-ctest . -C RelWithDebInfo
+ctest . -C RelWithDebInfo || goto error
 cd %GITHUB_WORKSPACE%\mama\jni || goto error
-gradle test
+gradle test || goto error
 cd %GITHUB_WORKSPACE% || goto error
 7z a openmama-%VERSION%.win.%VCVER%.%PLATFORM%.zip "%OPENMAMA_INSTALL_DIR%" || goto error
 
