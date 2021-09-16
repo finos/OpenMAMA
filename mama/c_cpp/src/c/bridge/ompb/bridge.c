8b7c1d926b05734be9b6d7e1f90583aa337f654e
Frank Quinn <fquinn@cascadium.io>
2021-09-16 19:09:06 +0100

    Added new OpenMAMA Playback Bridge (ompb)
    This bridge will help allow users to test their applications without having
    to set up an out-of-process publisher to connect to. It can be built by adding
    the following option to cmake:
    
        -DWITH_OMPB_BRIDGE=ON
    
    And can then be used with `-m ompb` where the following mama.properties config
    parameters are supported (given a transport name of 'pb'):
    
        mama.ompb.transport.pb.file = /path/to/playback.mplay
        mama.ompb.transport.pb.rate = 100000
        mama.ompb.transport.pb.on_completion = rewind
    
    Note that you can also pass "raise" to the on_completion parameter to get the
    bridge to raise a SIGINT on playback completion which can then be caught in
    your application to clean up the test run.
    
    Signed-off-by: Frank Quinn <fquinn@cascadium.io>

---
+++ mama/c_cpp/src/c/bridge/ompb/bridge.c
@@ -0,0 +1,110 @@
+/*=========================================================================
+  =                             Includes                                  =
+  =========================================================================*/
+
+#include <mama/mama.h>
+#include <mama/version.h>
+#include <timers.h>
+#include <wombat/strutils.h>
+#include <mama/integration/mama.h>
+#include "bridgefunctions.h"
+
+
+/*=========================================================================
+  =                Typedefs, structs, enums and globals                   =
+  =========================================================================*/
+
+/* Version identifiers */
+#define             BRIDGE_NAME            "ompb"
+#define             BRIDGE_VERSION         "1.0"
+
+/* Default payload names and IDs to be loaded when this bridge is loaded */
+static char*        PAYLOAD_NAMES[]         =   { "qpidmsg", NULL };
+static char         PAYLOAD_IDS[]           =   { MAMA_PAYLOAD_QPID, '\0' };
+
+
+/*=========================================================================
+  =                              Macros                                   =
+  =========================================================================*/
+
+
+/*=========================================================================
+  =               Public interface implementation functions               =
+  =========================================================================*/
+
+/* Must be immplemented in overriding bridge */
+mama_status
+ompbBridge_init (mamaBridge bridgeImpl)
+{
+    mama_status status         = MAMA_STATUS_OK;
+    const char* runtimeVersion = NULL;
+
+    /* Reusable buffer to populate with property values */
+    char propString[MAX_INTERNAL_PROP_LEN];
+    versionInfo rtVer;
+
+    mama_log (MAMA_LOG_LEVEL_SEVERE, "ompbBridge_init(): IN INIT");
+
+    /* Will set the bridge's compile time MAMA version */
+    MAMA_SET_BRIDGE_COMPILE_TIME_VERSION(BRIDGE_NAME);
+
+    /* Enable extending of the base bridge implementation */
+    status = mamaBridgeImpl_setReadOnlyProperty (bridgeImpl,
+                                                 MAMA_PROP_EXTENDS_BASE_BRIDGE,
+                                                 "true");
+
+    /* Get the runtime version of MAMA and parse into version struct */
+    runtimeVersion = mamaInternal_getMetaProperty (MAMA_PROP_MAMA_RUNTIME_VER);
+    strToVersionInfo (runtimeVersion, &rtVer);
+
+    /* NB checks are runtime only - assume build system will prevent accidental
+     * compilation against incompatible versions. This is a demonstration to
+     * show how you could do runtime version checking. */
+    if (1 == rtVer.mMajor)
+    {
+        mama_log (MAMA_LOG_LEVEL_SEVERE, "ompbBridge_init(): "
+                                         "This version of the bridge (%s) cannot be used with MAMA %s.",
+                  BRIDGE_VERSION,
+                  runtimeVersion);
+        return MAMA_STATUS_NOT_IMPLEMENTED;
+    }
+
+    return status;
+}
+
+/* Must be immplemented in overriding bridge */
+const char*
+ompbBridge_getVersion (void)
+{
+    return BRIDGE_VERSION;
+}
+
+/* Must be immplemented in overriding bridge */
+const char*
+ompbBridge_getName (void)
+{
+    return BRIDGE_NAME;
+}
+
+/* Must be immplemented in overriding bridge */
+mama_status
+ompbBridge_getDefaultPayloadId (char ***name, char **id)
+{
+    if (NULL == name || NULL == id)
+    {
+        return MAMA_STATUS_NULL_ARG;
+    }
+    /*
+     * Populate name with the value of all supported payload names, the first
+     * being the default
+     */
+    *name   = PAYLOAD_NAMES;
+
+    /*
+     * Populate id with the char keys for all supported payload names, the first
+     * being the default
+     */
+    *id     = PAYLOAD_IDS;
+
+    return MAMA_STATUS_OK;
+}
