88ff05b61dd1ddffbbd72a375ec7d60db2d6f9a2
Frank Quinn <fquinn@cascadium.io>
2022-06-22 21:58:22 +0100

    Updated base bridge to isolate IO implementation
    Signed-off-by: Frank Quinn <fquinn@cascadium.io>

--- mama/c_cpp/src/c/bridge/base/basedefs.h
+++ mama/c_cpp/src/c/bridge/base/basedefs.h
@@ -31,21 +31,12 @@ extern "C" {
   =                Typedefs, structs, enums and globals                   =
   =========================================================================*/
 
-typedef struct baseBridgeIoImpl
-{
-    struct event_base*  mEventBase;
-    wthread_t           mDispatchThread;
-    uint8_t             mActive;
-    uint8_t             mEventsRegistered;
-    wsem_t              mResumeDispatching;
-} baseBridgeIoImpl;
-
 typedef struct baseBridgeClosure_
 {
     // Note that mClosure is first - contains implementation bridge's own closure
     void*                 mImplClosure;
     timerHeap             mTimerHeap;
-    baseBridgeIoImpl      mIoState;
+    void*                 mIoState;
 } baseBridgeClosure;
 
 #if defined(__cplusplus)
