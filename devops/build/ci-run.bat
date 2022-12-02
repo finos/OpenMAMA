e66e75646d6275bcb1f16550db21c27c7ed45e48
Frank Quinn <fquinn@cascadium.io>
2022-12-02 17:02:04 +0000

    Pruned choco involvement

--- devops/build/ci-run.bat
+++ devops/build/ci-run.bat
@@ -3,7 +3,7 @@ set OPENMAMA_INSTALL_DIR=%GITHUB_WORKSPACE%\openmama-%VERSION%.win.%PLATFORM%
 
 echo "VCPKG_DEFAULT_BINARY_CACHE=%VCPKG_DEFAULT_BINARY_CACHE%"
 cmake --version
-choco install --no-progress -y unzip gow cmake || goto error
+choco install --no-progress -y gow || goto error
 REM Set PATH before adding to it since it's absurdly long in github actions and complains if we try and append to it
 set PATH=C:\Program Files (x86)\Gow\bin;%JAVA_HOME%;C:\Program Files\CMake\bin;C:\Program Files\Docker;C:\Program Files\Git\bin;C:\Program Files\Git\cmd;C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin;C:\Program Files\Java\jdk8u275-b01\bin;C:\Program Files\OpenSSL\bin;C:\Program Files\PowerShell\7\;C:\Program Files\TortoiseSVN\bin;C:\Program Files\dotnet;C:\ProgramData\Chocolatey\bin;C:\ProgramData\chocolatey\lib\ghc.8.10.2.2\tools\ghc-8.10.2\bin;C:\ProgramData\chocolatey\lib\maven\apache-maven-3.6.3\bin;C:\ProgramData\chocolatey\lib\pulumi\tools\Pulumi\bin;C:\ProgramData\kind;C:\hostedtoolcache\windows\Python\3.7.9\x64;C:\hostedtoolcache\windows\Python\3.7.9\x64\Scripts;C:\hostedtoolcache\windows\Ruby\2.5.8\x64\bin;C:\hostedtoolcache\windows\go\1.14.13\x64\bin;C:\hostedtoolcache\windows\stack\2.5.1\x64;C:\mysql-5.7.21-winx64\bin;C:\npm\prefix;C:\vcpkg;C:\windows;C:\windows\System32\Wbem;C:\windows\System32\WindowsPowerShell\v1.0\;C:\windows\system32;c:\tools\php
 mkdir %GITHUB_WORKSPACE%\build || goto error
