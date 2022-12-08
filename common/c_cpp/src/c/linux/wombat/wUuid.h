8c70fb6bf9fd8e58c2be462de9235cbfc1268901
fquinner <fquinn@cascadium.io>
2022-12-08 19:47:24 +0000

    Fixed wUuid header on OSX for C++

--- common/c_cpp/src/c/linux/wombat/wUuid.h
+++ common/c_cpp/src/c/linux/wombat/wUuid.h
@@ -25,6 +25,11 @@
 
 #include <uuid/uuid.h>
 
+#if defined (__cplusplus)
+extern "C"
+{
+#endif
+
 typedef uuid_t wUuid;
 
 #define WUUID_UNPARSE_OUTPUT_LENGTH 37
@@ -37,5 +42,9 @@ typedef uuid_t wUuid;
 #define wUuid_unparse uuid_unparse
 
 #define wUuid_clear(UUID) uuid_clear(UUID)
+
+#if defined (__cplusplus)
+} /* extern "c" */
+#endif
 
 #endif /* WUUID_H__ */
