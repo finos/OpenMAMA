50eb5b77ba58706f3eed0bbbec14380e20bf1e64
Frank Quinn <fquinn@cascadium.io>
2023-09-19 15:55:54 +0100

    Increased ctest timeout

--- devops/build/ci-run.sh
+++ devops/build/ci-run.sh
@@ -35,7 +35,7 @@ cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo \
      "$SOURCE_PATH_ABSOLUTE"
 make -j install
 export LD_LIBRARY_PATH=/opt/openmama/lib
-ctest . --timeout 240 --output-on-failure
+ctest . --timeout 300 --output-on-failure
 cd "$SOURCE_PATH_ABSOLUTE" > /dev/null
 
 # Include the test data and grab profile configuration from there for packaging
