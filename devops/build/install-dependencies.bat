7c24c2f0613d2f8f3612a7a5fa9abe342816aec2
Frank Quinn <fquinn@cascadium.io>
2023-09-19 14:16:21 +0100

    Bumped vcpkg version since apr out of date

--- devops/build/install-dependencies.bat
+++ devops/build/install-dependencies.bat
@@ -1,8 +1,8 @@
 REM Output will be written to current package
 git clone https://github.com/microsoft/vcpkg.git vcpkg-openmama || goto error
 cd vcpkg-openmama || goto error
+git checkout 2023.08.09 || goto error
 call .\bootstrap-vcpkg.bat || goto error
-git checkout 62d01b70df227850b728f5050418b917ad6d2b32 || goto error
 vcpkg install --triplet %PLATFORM%-windows qpid-proton libevent apr apr-util gtest || goto error
 
 goto end
